14d045db19e0e2e4b1da33242ed9d86fb906bcd9
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-09-01 12:29:54 +0300

    HW6 PM2_Mdlwr_dotenv

--- HW5/config/variables.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOOSE_CONNECT: 'mongodb://localhost:27017/apr-2021-okten',
-    PORT: 5000
-};
---
+++ HW6/config/variables.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB_CONNECTION_URL: process.env.DB_CONNECTION_URL || 'mongodb://localhost:27017/apr-2021-okten',
+    PORT: process.env.PORT || 5000
+};
